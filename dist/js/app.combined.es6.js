17cd34e3cf4a3db05007eafcf04a16e9f7e3ec0c
Scott Rippey <scott.william.rippey@gmail.com>
2015-05-30 23:27:09 -0700

    [title-bar] Right-aligned search box

--- dist/js/app.combined.es6.js
+++ dist/js/app.combined.es6.js
@@ -36,18 +36,22 @@ angular.module('title-bar.js', [])
 			template: `
 				<nav class="title-bar">
 					<div class="title-bar-contents">
-						<span class="simply-social-logo">
-							<span class="svg-logo" ng-include="'dist/images/simply-social-logo.svg'"></span>
-							<span class="simply">simply</span><span class="social">social<span>
+						<span class="title-group-one">
+							<span class="simply-social-logo">
+								<span class="svg-logo" ng-include="'dist/images/simply-social-logo.svg'"></span>
+								<span class="simply">simply</span><span class="social">social</span>
+							</span>
+						</span>
+						<span class="title-group-two">
+							<span class="svg-caption-add" ng-include="'dist/images/caption-add.svg'"></span>
+							
+							<label class="search-box">
+								<input type="text">
+								<span class="svg-search-glass" ng-include="'dist/images/search-glass.svg'"></span>
+							</label>
+							<current-user-avatar></current-user-avatar>
 						</span>
 						
-						<span class="svg-caption-add" ng-include="'dist/images/caption-add.svg'"></span>
-						
-						<label class="search-box">
-							<input type="text">
-							<span class="svg-search-glass" ng-include="'dist/images/search-glass.svg'"></span>
-						</label>
-						<current-user-avatar></current-user-avatar>
 					</div>
 				</nav>
 			`
