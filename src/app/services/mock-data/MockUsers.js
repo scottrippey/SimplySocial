beeee2b29e0bc3bb664fe1872fefc8dd1e460714
Scott Rippey <scott.william.rippey@gmail.com>
2015-06-01 05:17:34 -0700

    [settings] added settings page

--- src/app/services/mock-data/MockUsers.js
+++ src/app/services/mock-data/MockUsers.js
@@ -5,7 +5,7 @@ angular.module('MockUsers.js', [])
 			sam: { name: "Sam Soffes", avatarUrl: "//lorempixel.com/100/100/people/1" },
 			jed: { name: "Jed Bridges", avatarUrl: "//lorempixel.com/100/100/people/2" },
 			ren: { name: "Ren Walker", avatarUrl: "//lorempixel.com/100/100/people/3" },
-			meg: { name: "Meg Robichaud", avatarUrl: "//lorempixel.com/100/100/people/4" },
+			meg: { name: "Meg Robichaud", avatarUrl: "//lorempixel.com/100/100/people/4", email: "meg.robichaud@email.com" },
 		};
 		
 		return MockUsers;
