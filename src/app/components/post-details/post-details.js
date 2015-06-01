985cf089bbc6edd0d74b0bb34b8aeac64f1beb58
Scott Rippey <scott.william.rippey@gmail.com>
2015-06-01 02:02:08 -0700

    [post] Layout

--- src/app/components/post-details/post-details.js
+++ src/app/components/post-details/post-details.js
@@ -11,30 +11,29 @@ angular.module('post-details.js',[])
 			template: `
 				<article class="post-details">
 				
-					<user-avatar class="post-avatar" user="vm.post.user"></user-avatar>
-					<img class="user-avatar" ng-src="{{vm.post.user.avatarUrl}}">
+					<img class="post-avatar" ng-src="{{vm.post.user.avatarUrl}}">
 					
-					<header>
-						<h3> {{ vm.post.user.name }} </h3>
-						<span class="post-actions">
-							<span class="svg-reply" ng-include="'dist/images/reply.svg'"> (reply) </span>
-							<span class="svg-heart" ng-include="'dist/images/heart.svg'"> (heart) </span>
-							<span class="post-time" >
-								{{ vm.post.timestamp }}
-							</span>
+					<div class="post-actions">
+						<span class="svg-reply" ng-include="'dist/images/reply.svg'"> (reply) </span>
+						<span class="svg-heart" ng-include="'dist/images/heart.svg'"> (heart) </span>
+						<span class="post-time" >
+							{{ vm.post.timestamp }}
 						</span>
+					</div>
+					<header class="post-header">
+						<a class="post-user-name"> {{ vm.post.user.name }} </a>
 					</header>
 					
-					<p>
+					<p class="post-message">
 						{{ vm.post.message }}
 					</p>
 					
 					<div class="post-media" ng-if="vm.post.multimedia">
 						<div class="post-media-image" ng-if="vm.post.multimedia.mediaType == 'photo'">
-							<img ng-src="{{vm.post.multimedia.imageUrl}}">
+							<img class="post-media-item" ng-src="{{vm.post.multimedia.imageUrl}}">
 						</div>
 						<div class="post-media-video" ng-if="vm.post.multimedia.mediaType == 'video'">
-							<video controls ng-attr-poster="{{ vm.post.multimedia.poster }}">
+							<video class="post-media-item" controls ng-attr-poster="{{ vm.post.multimedia.poster }}">
 								<source 
 									ng-repeat="source in vm.post.multimedia.videos"
 									ng-src="{{ source.videoUrl | trusted }}" 
@@ -44,7 +43,7 @@ angular.module('post-details.js',[])
 						</div>
 					</div>
 					
-					<footer ng-if="vm.post.replies">
+					<footer class="post-replies" ng-if="vm.post.replies">
 						Expand (v)
 						<post-reply ng-repeat="reply in vm.post.replies">
 							{{ reply.user.name }} - {{ reply.message }}
